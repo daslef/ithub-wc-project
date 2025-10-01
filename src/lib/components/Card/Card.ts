c06f5a2d64d31e9238742c8e28413106bd21aba1
user <user@E4299.ithub.loc>
2025-10-01 17:17:19 +0300

    wip: card

--- src/lib/components/Card/Card.ts
+++ src/lib/components/Card/Card.ts
@@ -1,5 +1,29 @@
 export default class CardComponent extends HTMLElement {
+  private template;
 
+  constructor() {
+    super();
+
+    this.template = document.createElement("template");
+    this.template.innerHTML = `
+      <style>
+        .card {
+          min-height: 377px;
+          width: 320px;
+          border-radius: 16px;
+          background-color: white;
+          box-shadow: var(--shadow);
+        }
+      </style>
+
+      <article class="card">
+    
+      </article>
+    `;
+
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot?.appendChild(this.template.content.cloneNode(true));
+  }
 }
 
-customElements.define("card-component", CardComponent)
+customElements.define("card-component", CardComponent);
